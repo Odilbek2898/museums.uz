56e283af39607e9be6fa3e9647a83cbb8bb3dbd5
Odilbek <59110544+Odilbek2898@users.noreply.github.com>
2020-09-10 16:53:10 +0500

    Update wsgi.py

--- museumBlog/museumBlog/wsgi.py
+++ museumBlog/museumBlog/wsgi.py
@@ -1,12 +1,9 @@
 import os
 
 from django.core.wsgi import get_wsgi_application
-from dj_static import Cling
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'museumBlog.settings')
 
 application = get_wsgi_application()
 
-if os.getcwd() == '/blogapp':
-    from whitenoise.django import DjangoWhiteNoise
-    application = DjangoWhiteNoise(application)
+
